85bce841b4619db1256c5f70ffc1d8de83b0fa0e
Krzysztof Noga <>
2020-09-25 16:02:54 +0200

    gp-tracker build

---
+++ gp-tracker/precache-manifest.38c75d287112d903de3af33d4e64cf32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32aeeae34710f95b54ea69d5251bcfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14f14f1a365ed7160f6f",
+    "url": "/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "03fe2616dc55fbc87b77",
+    "url": "/static/css/main.88aa7823.chunk.css"
+  },
+  {
+    "revision": "14f14f1a365ed7160f6f",
+    "url": "/static/js/2.e9d7e6a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e9d7e6a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03fe2616dc55fbc87b77",
+    "url": "/static/js/main.1bfb0c73.chunk.js"
+  },
+  {
+    "revision": "68a1249a7e8244c27120",
+    "url": "/static/js/runtime-main.0a757917.js"
+  }
+]);
